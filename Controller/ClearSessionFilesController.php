f93b616837030320b53a20a3d0992f6a7c31952a
Lukas Marks <lukas.marks@lumax-web.de>
2015-05-19 12:16:10 +0200

    Refactoring

--- Controller/ClearSessionFilesController.php
+++ Controller/ClearSessionFilesController.php
@@ -11,14 +11,31 @@
  * @since         0.1
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 App::uses('ClearSessionFilesAppController', 'ClearSessionFile.Controller');
 
 class ClearSessionFilesController extends ClearSessionFilesAppController {
+	
+	/**
+	 * Models used by the Controller
+	 *
+	 * @var array
+	 * @access public
+	 */ 
 	public $uses = array('ClearSessionFile.ClearSessionFile');
-	public function admin_clear() { 
-		$this->ClearSessionFile->delete();
-		$this->Session->setFlash(__d('clear_session_file', 'Session files have been deleted, please login.'), 'default', array('class' => 'success'));
+	
+	/**
+	 * Admin clear
+	 *
+	 * @return void
+	 * @access public
+	 */
+	public function admin_clear() {
+		if ($this->{$this->modelClass}->delete()) {
+			$this->Session->setFlash(__d('clear_session_file', 'Session files have been deleted, please login.'), 'flash', array('class' => 'success'));
+			return $this->redirect(Configure::read('Croogo.dashboardUrl'));
+		}
+		
+		$this->Session->setFlash(__d('clear_session_file', 'An error occurred. Please, try again.'), 'flash', array('class' => 'error'));
 		return $this->redirect(Configure::read('Croogo.dashboardUrl'));
 	}
 }
